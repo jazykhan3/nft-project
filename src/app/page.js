aa61266998787d07ebc8859cacb830135ec396f2
JahanZaib Sultan <80755940+jazykhan3@users.noreply.github.com>
2025-04-30 18:04:58 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import FrontPage from "./home/page";
+import FrontPageV2 from "./v2/page";
 
 export default function Home() {
   return (
   <>
-  <FrontPage />
+  <FrontPageV2 />
   {/* <DefiFund /> */}
   </>
   );
